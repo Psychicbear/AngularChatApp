4fec4ce13b920d0bf85a84c134a7625149834739
Psychicbear <50544746+Psychicbear@users.noreply.github.com>
2023-08-22 18:30:25 +1000

    Added basic routing structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChannelsComponent } from './channels/channels.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GroupsComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'groups/:groupid', component: ChannelsComponent},
+  {path: 'channels/:channelid', component: ChatComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
