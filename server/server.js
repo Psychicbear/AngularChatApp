aac7ed579c93489f59b17d9ade4a5d5b1d422bd1
Psychicbear <50544746+Psychicbear@users.noreply.github.com>
2023-08-28 16:06:18 +1000

    Added bootstrap, created login/register page with functional endpoints, implemented basic account page

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 let app = require('express')()
 let cors = require('cors')
 let http = require('http').Server(app)
-const fs = require('fs')
+const fs = require('fs/promises')
+let uuid = require('uuid').v4
 const bodyparser = require('body-parser')
 let port = 3000;
 app.use(cors())
@@ -10,22 +11,64 @@ app.listen(port, () => {
     console.log(`Launched local server at port ${port}`)
 })
 
-// Takes data = {email: string, password: string}, sends {success: boolean} to client
-app.post('/api/login', (req, res) => {
-    let creds = req.body
-    console.log(creds)
+
+app.post('/api/updateme', async (req, res) => {
+    let user = req.body
     let validate = {success: false}
-    fs.readFile('data.json', (err, data) => {
-        let file = JSON.parse(data)
-        console.log(file)
+    try {
+        let file = JSON.parse(await fs.readFile('data.json'))
+        let idx = file.users.findIndex(val => {
+            return val.id == user.id
+        })
+        file.users[idx] = user
+        await fs.writeFile('data.json', JSON.stringify(file))
+        validate.success = true
+    } catch (err) {
+        console.log(`Error occurred: ${err}`)
+    } finally {
+        res.send(validate)
+    }
+})
+
+
+// Takes data = {email: string, password: string}, sends {success: boolean} to client
+app.post('/api/login', async (req, res) => {
+    let creds = req.body
+    let validate = {success: false}
+    try {
+        let file = JSON.parse(await fs.readFile('data.json'))
         let authUser = file.users.find((value) => {
             return value.email == creds.email && value.password == creds.password
         })
-        console.log(authUser)
-        if(authUser){
-            validate.success = true
-        }
-        console.log(validate.success)
+        const {password, ...response} = authUser
+        validate.success = true
+        validate = {...response ,...validate}
+    } catch (err) {
+        console.log(`Error occurred: ${err}`)
+    } finally {
         res.send(validate)
-    })
+    }
+})
+
+app.post('/api/register', async (req, res) => {
+    let creds = req.body
+    let validate = {success: false}
+    try {
+        let file = JSON.parse(await fs.readFile('data.json'))
+        if(file.users.find((val) => { return val.username == creds.username || val.email == creds.email})){
+            throw "Account already exists with this username or email"
+        } else {
+            let newuser = {...creds, id: uuid(), roles: [], groups: []}
+            console.log(newuser)
+            file.users.push(newuser)
+            await fs.writeFile('data.json', JSON.stringify(file))
+            const {password, ...valuser} = newuser
+            validate = {...valuser, success: true}
+        }
+    } catch(err) {
+        console.log(`Error occurred: ${err}`)
+        validate = {...validate, error: err}
+    } finally {
+        res.send(validate)
+    }
 })
